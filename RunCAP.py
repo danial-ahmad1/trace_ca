2ea2b9b48a8b575e1f4789eb988eacfb3ca1ccee
Dan Ahmad <sdanialahmad1@gmail.com>
2024-07-08 15:52:11 -0400

    Added meta analysis

--- RunCAP.py
+++ RunCAP.py
@@ -21,6 +21,15 @@ def pull_files(folder_path):
 
     return files
 
+def flatten(lst):
+    flat_list = []
+    for item in lst:
+        if isinstance(item, list):
+            flat_list.extend(flatten(item))
+        else:
+            flat_list.append(item)
+    return flat_list
+
 folder_loc = select_folder()
 files_list = pull_files(folder_loc)
 
@@ -31,15 +40,33 @@ files_expname = []
 files_expname = [f.split('_')[0] for f in files_list]
 files_expname_unique = list(set(files_expname))
 
+# Number of detections
 meta_analysis = {}
 for expname in files_expname_unique:
     meta_analysis[expname] = []
+
+# Number of detections per area
+meta_analysis2 = {}
+for expname in files_expname_unique:
+    meta_analysis2[expname] = []
+
+# All detection sizes per image
+meta_analysis3 = {}
+for expname in files_expname_unique:
+    meta_analysis3[expname] = []
 
 for file in files_list:
     selected_path = folder_loc + '/' + file
     CA_processor = cap.CAProcessor(selected_path)
     CA_processor.run_CAProcessor()
     meta_analysis[CA_processor.file_name_trunc].append(len(CA_processor.region_im_filtered))
+
+    if CA_processor.area_sum > 0:
+        meta_analysis2[CA_processor.file_name_trunc].append(len(CA_processor.region_im_filtered)/CA_processor.area_sum)
+    elif CA_processor.area_sum == 0:
+        meta_analysis2[CA_processor.file_name_trunc].append(0)
+
+    meta_analysis3[CA_processor.file_name_trunc].append(flatten(CA_processor.area_list))
 
     # CA_processor.run_CAPDebug_fcn1()
     # CA_processor.run_CAPDebug_fcn2()
@@ -49,6 +76,15 @@ for file in files_list:
 meta_df = pd.DataFrame(meta_analysis)
 meta_df.to_csv('/Users/moose/Desktop/trace_ca-local/Computed_Results' + '/meta_analysis.csv', index=False)
 print(meta_df)
+
+meta2_df = pd.DataFrame(meta_analysis2)
+meta2_df.to_csv('/Users/moose/Desktop/trace_ca-local/Computed_Results' + '/meta_analysis2.csv', index=False)
+print(meta2_df)
+
+meta_analysis3 = {k: flatten(v) for k, v in meta_analysis3.items()}
+meta3_df = pd.DataFrame(meta_analysis3)
+meta3_df.to_csv('/Users/moose/Desktop/trace_ca-local/Computed_Results' + '/meta_analysis3.csv', index=False)
+print(meta3_df)
 
 # print(meta_analysis)
 # If in Jupyter Notebook, use the following to clear memory
